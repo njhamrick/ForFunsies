9bd8d3db92b411fabfb1fa26253177e6fe2bdf43
njhamrick <njudithhamrick@gmail.com>
2026-08-24 04:42:36 -0600

    added content

--- terminal_gremlin.py
+++ terminal_gremlin.py
@@ -6,3 +6,18 @@ name = input("State your name, mortal:")
 
 print(f"\nHmm... {name}. ")
 print("The terminal has acknowledged your presence.")
+
+mood = int(input("\nHow emotionally stable are you from 1-10? "))
+
+if mood <= 3:
+      print("Oh.")
+       print("That number came with a distress signal.")
+
+elif mood <= 7:
+      print("Hmm. I see.")
+      print("The gremlin is watching.")
+
+else:
+        print("Ah, a happy mortal!")
+        print("The gremlin is pleased with your emotional state.")  
+

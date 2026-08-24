f3f9ddfb303c32fc31576abab954d2ea8703c916
njhamrick <njudithhamrick@gmail.com>
2026-08-24 04:30:57 -0600

    added content

---
+++ terminal_gremlin.py
@@ -0,0 +1,8 @@
+print("=======================================")
+print("           TERMINAL GREMLIN V1.0")
+print("=======================================")
+
+name = input("State your name, mortal:")
+
+print(f"\nHmm... {name}. ")
+print("The terminal has acknowledged your presence.")
